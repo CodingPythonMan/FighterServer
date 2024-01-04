2581c4bb2e9a62c6e6004633d1baefaffbc915c2
JuYoung Kim <robolab1902@naver.com>
2024-01-04 18:49:04 +0900

    3주 후에 제출해야하는 서버라서 지속적으로 제출해야한다.

--- Log.cpp
+++ Log.cpp
@@ -1,1 +1,11 @@
 #include "Log.h"
+#include <stdio.h>
+
+int gLogLevel;
+
+void Log(WCHAR* String, int LogLevel)
+{
+	wprintf(L"%s \n", String);
+
+	//if(LogLevel)
+}
--- Log.h
+++ Log.h
@@ -1,5 +1,15 @@
 #pragma once
-class Log
-{
+#include <windows.h>
 
-};
+#define LOG_LEVEL_DEBUG 0
+#define LOG_LEVEL_ERROR 1
+#define LOG_LEVEL_SYSTEM 2
+
+void Log(WCHAR* String, int LogLevel);
+
+void Log(WCHAR* String);
+
+#define LOG_MACRO {			\
+							\
+							\
+}							
